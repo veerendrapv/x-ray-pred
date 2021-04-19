872d748d0f3ceb032130bd2d1a9c2fdb03e9e662
veerendrapv <39883124+veerendrapv@users.noreply.github.com>
2021-04-19 22:14:17 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,9 +1,9 @@
 def predict(model_path, image_path):
     from torch import nn
     import torch
-    import cv2
+#     import cv2
     import numpy as np
-    from skimage import io
+    from skimage import io, transform
     index_to_name = {0:'Covid', 1:'Normal', 2:'Viral Pnuemonia'}
     criterion = nn.CrossEntropyLoss()
     
@@ -12,7 +12,7 @@ def predict(model_path, image_path):
     else:
         device = 'cpu'
     image = io.imread(image_path)
-    image = np.float32(cv2.resize(image, (224, 224))) / 255
+    image = np.float32(transform.resize(image, (224, 224))) / 255
     means = np.array([0.485, 0.456, 0.406])
     stds = np.array([0.229, 0.224, 0.225])
     image -= means
